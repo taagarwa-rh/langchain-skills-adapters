916f8e1f6d8fac8ff1e9465591f2e87d9de7b38e
Taylor Agarwal <taagarwa@redhat.com>
2026-03-20 14:39:21 -0400

    🐛 Add __version__ to package

--- src/langchain_skills_adapters/__init__.py
+++ src/langchain_skills_adapters/__init__.py
@@ -1,5 +1,9 @@
 """Langchain skills adapters."""
 
+from importlib.metadata import version
+
+__version__ = version("langchain-skills-adapters")
+
 from .core import Skill, SkillCatalog, SkillsLoader
 from .middleware import SkillsMiddleware
 from .tools import SkillsTool
